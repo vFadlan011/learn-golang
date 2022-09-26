ab9da4f7eec8b4a235917519b1e1199abc2a1fb7
Fadlan A <fadlanerman6@gmail.com>
2022-09-27 02:45:49 +0700

    init commit before pushed to github

--- 03-variables/03-variables.go
+++ 03-variables/03-variables.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	// deklarasi variable dengan menuliskan tipe data secara eksplisit
 	var firstName string = "john"
-	// deklarasi variable tanpa menuliskan tipe data
+	// deklarasi variable tanpa menuliskan tipe data (disebut type inference, untuk best practicenya di google banyak)
 	var lastName = "doe"
 	// deklarasi variable tanpa menggunakan var
 	fullName := firstName + " " + lastName
@@ -15,10 +15,21 @@ func main() {
 	lastName = "dane"
 	fmt.Println(lastName)
 
-	/* Deklasrasi multi-variable
-	dapat dilakukan dengan menuliskan atau tidak menuliskan tipe data secara eksplisit
-	tidak perlu menuliskan tipe data secara eksplisit jika dalam satu baris deklarasi
-	terdapat tipe data yang berbeda */
+	// Deklasrasi multi-variable
+	var five, six, seven int8 = 5, 6, 7
+	fmt.Println(five, six, seven)
+	/*
+		dapat dilakukan dengan menuliskan atau tidak menuliskan tipe data secara eksplisit
+		tidak perlu menuliskan tipe data secara eksplisit jika dalam satu baris deklarasi
+		terdapat tipe data yang berbeda */
 	var the, quick, fox, two, threePointThree = "the", "quick", "fox", 2, 3.3
 	fmt.Println(the, quick, fox, two, threePointThree)
+
+	// Deklarasi multi-variable yang lebih ringkas
+	today, is, dayName, date, monthName := "today", "is", "Tuesday,", 27, "sept"
+	fmt.Println(today, is, dayName, date, monthName)
+
+	// variable _ dalam go
+	// digunakan untuk menampung nilai yang tidak terpakai
+	_ = "something that useless"
 }
---
+++ 04-constant-variables/go.mod
@@ -0,0 +1,3 @@
+module constant-variables
+
+go 1.18
