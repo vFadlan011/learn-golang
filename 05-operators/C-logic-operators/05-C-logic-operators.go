18ebcac6e9246d260cf577cb3e1e8779568cd6d1
Fadlan A <fadlanerman6@gmail.com>
2022-09-28 06:23:58 +0700

    progress 05-C-logic-operators

--- 05-operators/C-logic-operators/05-C-logic-operators.go
+++ 05-operators/C-logic-operators/05-C-logic-operators.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("\n&& (and)")
+	fmt.Println(" true && true\t:", true && true)   //true
+	fmt.Println(" true && false\t:", true && false) //false
+
+	fmt.Println("\n|| (or)")
+	fmt.Println(" true || true\t:", true || true)    //true
+	fmt.Println(" true || false\t:", true || false)  //true
+	fmt.Println("false || false\t:", false || false) //true
+
+	fmt.Println("\n! (not/negation)")
+	fmt.Println("!true\t\t:", !true)   //false
+	fmt.Println("!false\t\t:", !false) //true
+
+	fmt.Println("\nMultiple logic operators")
+	fmt.Println("(27 <= 32) && (32 > 48)\t:", (27 <= 32) && (48 > 32)) //true
+	fmt.Println("(27 <= 32) && (32 > 48)\t:", (27 <= 32) && (48 < 32)) //false
+	fmt.Println("(27 <= 32) && (32 > 48)\t:", (27 <= 32) || (48 > 32)) //true
+	fmt.Println("(27 <= 32) && (32 < 48)\t:", (27 <= 32) || (48 < 32)) //true
 }
